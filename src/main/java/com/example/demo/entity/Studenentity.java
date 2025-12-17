68403542132803c92754b019007464d22cdc80ee
bala-amypo <bala@amypo.in>
2025-12-17 04:08:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studenentity.java
+++ src/main/java/com/example/demo/entity/Studenentity.java
@@ -2,12 +2,26 @@
 
 public class Studentity{
 
-    private Long id;
+    private int id;
     private String name;
     private String email;
     private float cgpa;
 
-    void 
+    public Studentity(int id, String name, String email, float cgpa){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.cgpa = cgpa;
+    }
+
+    public Studentity(){
+
+    }
+
+    public int getId(){
+        this.id = id;
+        
+    }
 
 
 }
